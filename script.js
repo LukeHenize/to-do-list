42400f8ae40f7ff232f8a4d2ef73910bae127c95
Luke Henize <47581739+lhenize5@users.noreply.github.com>
2021-07-07 12:36:14 -0400

    finalized mobile-friendly overlay, began timer functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function addItem() {
     if(!isFormComplete()) {
-        alert("Please fill out all fields to add a new list item!");
+        alert(`Please fill out the "Task Name" field!`);
     }else {
         //create a div with class item to hold item info
         const item = document.createElement("div");
@@ -12,6 +12,22 @@ function addItem() {
         let text = document.createTextNode(input);
         itemTitle.appendChild(text);
 
+        //create <p> to hold description (optional)
+        let info = document.getElementById("add-desc");
+        const description = document.createElement("p");
+        if(info.value != '') {
+            input = info.value;
+            text = document.createTextNode(input);
+            description.appendChild(text);
+            description.style.padding = "0px 15px 0px 15px";
+        } 
+
+        //create timer to track task time
+        const timer = document.createElement("h3");
+        const date = new Date();
+        text = document.createTextNode("Started 0 minutes ago");
+        timer.appendChild(text);
+
         //create complete button and add onclick functionality and text
         const completeButton = document.createElement("button");
         completeButton.classList.add("complete");
@@ -20,9 +36,14 @@ function addItem() {
         text = document.createTextNode("Complete Task");
         completeButton.appendChild(text);
 
-        //add h2 and button to earlier div
+        //add h2, p and button to earlier div
         item.appendChild(itemTitle);
+        item.appendChild(timer);
+        if(info.value != '') {
+            item.appendChild(description);
+        }
         item.appendChild(completeButton);
+        
 
         //add completed div to main list
         const list = document.getElementById("item-list");
@@ -30,6 +51,7 @@ function addItem() {
 
         //reset form values
         document.getElementById("add-title").value = '';
+        info.value = '';
         //update item counter
         updateCounter(1);
     }
@@ -41,7 +63,7 @@ function removeItem(eventObj) {
     updateCounter(-1);
 }
 
-function isFormComplete() { //returns true if all form fields are complete
+function isFormComplete() { //returns true if required form fields are complete
     let input = document.getElementById("add-title");
     if(input.value == '') {
         return false;
@@ -71,15 +93,31 @@ function updateCounter(num) {
     }
 }
 function toggleMenu() {
-    let sidebar = document.getElementById("sidebar");
-    if(sidebar.classList.contains("visible")) {
-        sidebar.classList.remove("visible");
-        sidebar.classList.add("hidden");   
-    }else {
-        sidebar.classList.remove("hidden");
-        sidebar.classList.add("visible");
+    if(window.innerWidth <= 625) {
+        let sidebar = document.getElementById("sidebar");
+        let overlay = document.getElementById("translucent");
+        if(sidebar.classList.contains("visible")) {
+            sidebar.classList.remove("visible");
+            sidebar.classList.add("hidden");   
+
+            overlay.style.display = "none";
+        }else {
+            sidebar.classList.remove("hidden");
+            sidebar.classList.add("visible");
+
+            overlay.style.display = "block";
+        }  
     }
-    
+}
+let timers = [];
+function createTimer() {
+    timers.push(0);
+}
+function updateTimers(arr) {
+    console.log(arr);
+    arr.forEach(timer => {
+       timer = timer + 5000;
+    });
 }
 function init() {
     const vw = document.documentElement.clientWidth;
@@ -91,3 +129,4 @@ function init() {
     }
 }
 window.onload = init;
+setInterval(updateTimers, 5000, timers);
